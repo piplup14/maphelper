13eb973c6996734ff6d614726abf63fe575442cc
piplup14 <rukai@bk.ru>
2022-02-11 11:38:28 +0300

    update

--- js.js
+++ js.js
@@ -60,15 +60,53 @@
         const canvas = document.createElement('canvas');
         canvas.id = 'canvas';
         placesPane.appendChild(canvas);
-        this.myMap.events.add('boundschange', this.redraw());
-       
-        
+        this.bindEvent();
         }
         destroy() {
-            // remove from div and free all
+        // remove from div and free all
+        }
+        bindEvent(){
+            const redr = this.redraw();
+            this.myMap.events.add('boundschange',redr /*(e) => {
+                let myMap = this.myMap;
+                let elements = this.elements;    
+                const projection = myMap.options.get('projection');
+            let newB = myMap.getBounds();
+            let zoom = myMap.getZoom();
+            let leftCorn = myMap.converter.globalToPage(
+            projection.toGlobalPixels(
+                newB[1],
+                zoom
+            )) 
+            if (canvas.getContext){
+                var ctx = canvas.getContext('2d');
+                ctx.canvas.width = 800;
+                ctx.canvas.height = 450;
+                
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                elements.forEach(element => {
+                    let place = myMap.converter.globalToPage(
+                        projection.toGlobalPixels(
+                            element,
+                            zoom
+                        ));
+                    ctx.beginPath();
+                place[0] = place[0]-(leftCorn[0]-800);
+                place[1] = place[1] - leftCorn[1];
+                ctx.arc(place[0],place[1],5,0,Math.PI*2,true);
+                ctx.stroke();
+                });
+                
+              }
+
+            }*/
+            
+            
+            );
         }
         redraw() {
             // full redraw all elements
+            console.log(this.myMap);
             const projection = this.myMap.options.get('projection');
             let newB = this.myMap.getBounds();
             let zoom = this.myMap.getZoom();
@@ -83,6 +121,7 @@
                 ctx.canvas.height = 450;
                 
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
+                console.log(this.elements);
                 this.elements.forEach(element => {
                     let place = this.myMap.converter.globalToPage(
                         projection.toGlobalPixels(
@@ -97,16 +136,34 @@
                 });
                 
               }
+              console.log(this.elements);
         }
         add(data) {
             this.elements.push(data);
         }
         clear() {
             this.elements = [];
-            this.redraw();
+            //this.redraw();
+            console.log('clear');
         }
     };
-/*
+
+
+function test() {
+    const map = new Map('map');
+    map.clear();
+    
+    //for (let i = 0; i < 1000; i++) {
+    map.add([52.9650800, 36.0784900]);
+    //}
+    console.log('121',map.elements);
+    map.redraw();
+    console.log('121',map.elements);
+}
+    ymaps.ready(test);
+
+
+    /*
     class Map {
         constructor() {
             this.myMap = new ymaps.Map("map", {
@@ -162,14 +219,3 @@
         }}
     }
 */
-
-
-function test() {
-    const map = new Map('map');
-    map.clear();
-    for (let i = 0; i < 1000; i++) {
-        map.add([53,37]);
-    }
-    map.redraw();
-}
-    ymaps.ready(test);
